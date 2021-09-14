a97ecdceebb8f864edfae48a8bac62318d15bf5d
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2021-09-14 12:53:10 +0300

    Login

--- front/src/app/_models/user.ts
+++ front/src/app/_models/user.ts
@@ -1,7 +1,13 @@
 export class User {
-    id: number;
-    username: string;
-    firstName: string;
-    lastName: string;
-    token?: string;
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    user: {
+        id: number;
+        name: string;
+        email: string;
+        email_verified_at?: string;
+        created_at: string;
+        updated_at: string;
+    }
 }
