1ec160e042e189f71d54fc2c1ea82efcd61106ff
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2021-10-17 14:33:55 +0300

    set games as homepage

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './_helpers';
 import { GameComponent } from './_views/game/game.component';
 import { GamesComponent } from './_views/games/games.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: GamesComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
 
-  { path: 'games', component: GamesComponent },
   { path: 'g/:gameId', component: GameComponent },
   { path: 'g/:gameId/:gameTitle', component: GameComponent },
 
